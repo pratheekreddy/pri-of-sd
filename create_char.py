6f887e7bc5abbd898fd51d15b40c5ef2a2a3a4a2
pratheek reddy <40834059+pratheekreddy@users.noreply.github.com>
2021-11-23 00:29:43 -0600

    AG-51 refactored exesting code

--- create_char.py
+++ create_char.py
@@ -2,10 +2,22 @@
 # position and hairstyle in an order to create a character 
 
 from action import action
+from create_item import create_item
 
-def create_char(name , body_type , clothing ,position, hair="Spiky"):
-    action('CreateCharacter('+name+','+body_type+')')
-    action('SetClothing('+name+', '+clothing+')')
-    action('SetExpression('+name+',Happy)')
-    action('SetHairStyle('+name+','+hair+')')
-    action('SetPosition('+name+','+position+')')
+class Create_char:
+
+    def __init__(self,name , body_type , clothing ,position, hair="Spiky"):
+        self.name = name
+        action('CreateCharacter('+name+','+body_type+')')
+        action('SetClothing('+name+', '+clothing+')')
+        action('SetExpression('+name+',Happy)')
+        action('SetHairStyle('+name+','+hair+')')
+        action('SetPosition('+name+','+position+')')
+    
+    def give_item(self,i_name,i_type):
+        create_item(i_name,i_type,self.name)
+        action('Draw('+self.name+','+i_name+')')
+
+    def camera(self):
+        action('SetCameraFocus('+self.name+')')
+
--- intro.py
+++ intro.py
@@ -1,5 +1,5 @@
 from action import action
-from create_char import create_char
+from create_char import Create_char
 from create_item import create_item
 
 def dream():
@@ -7,82 +7,66 @@ def dream():
     action('CreatePlace(Cottage, Cottage)')
     action('CreatePlace(Ruins,Ruins')
 
-    create_char('Charlie','D','Merchant','Cottage.Bed','Spiky')
+    global charlie_earth
+    charlie_earth=Create_char('Charlie','D','Merchant','Cottage.Bed','Spiky')
     
     action('Sleep(Charlie,Cottage.Bed)')
 
-    create_char('Charlie1','D','King','Ruins.Exit','Spiky')
+    global charlie_dream
+    charlie_dream=Create_char('Charlie1','D','King','Ruins.Exit','Spiky')
+    charlie_dream.give_item('Sword_charlie','Sword')
 
-    create_item('Sword','Sword','Charlie1')
-    action('Draw(Charlie1,Sword)')
+    soldier1=Create_char('Soldier1','D','HeavyArmour','Ruins.DirtPile')
+    soldier1.give_item('Sword_sol1','Sword')
 
-    create_char('Sol1','D','HeavyArmour','Ruins.DirtPile')
+    soldier2=Create_char('Soldier2','D','HeavyArmour','Ruins.Plant')
 
-    create_item('Sword1','Sword','Sol1')
-    action('Draw(Sol1,Sword1)')
+    soldier2.give_item('Sword_sol3','Sword','Sol2')
 
-    create_char('Sol2','D','HeavyArmour','Ruins.Plant')
+    soldier3=Create_char('Soldier3','D','HeavyArmour','Ruins.Altar')
 
-    create_item('Sword2','Sword','Sol2')
-    action('Draw(Sol2,Sword2)')
+    soldier3.give_item('Sword3','Sword','Sol3')
 
-    create_char('Sol3','D','HeavyArmour','Ruins.Altar')
+    soldier4=Create_char('Soldier4','D','HeavyArmour','Ruins.Throne')
 
-    create_item('Sword3','Sword','Sol3')
-    action('Draw(Sol3,Sword3)')
-
-    create_char('Sol4','D','HeavyArmour','Ruins.Throne')
-
-    create_item('Sword4','Sword','Sol4')
-    action('Draw(Sol4,Sword4)')
+    soldier4.give_item('Sword4','Sword','Sol4')
 
     action('Wait(1)')
     action('HideMenu()')
 
-    action('SetCameraFocus(Charlie1)')
+    charlie_dream.camera()
     action('SetCameraMode(Track)')
     action('SetCameraMode(Follow)')
     action('Wait(2)')
 
-    action('WalkTo(Charlie1, Sol1)')
-    action('PlaySound(Draw)','False')
-    action('Attack(Charlie1,Sol1)')
-    action('Die(Sol1)')
+    attack(charlie_dream.name,soldier1.name)
 
-    action('WalkTo(Charlie1,Sol2)')
-    action('PlaySound(Draw)','False')
-    action('Attack(Charlie1,Sol2)')
-    action('Die(Sol2)')
+    attack(charlie_dream.name,soldier2.name)
 
-    action('WalkTo(Charlie1,Sol3)')
-    action('PlaySound(Draw)','False')
-    action('Attack(Charlie1,Sol3)')
-    action('Die(Sol3)')
+    attack(charlie_dream.name,soldier3.name)
 
-    action('WalkTo(Charlie1,Sol4)')
-    action('PlaySound(Draw)','False')
-    action('Attack(Charlie1,Sol4)')
-    action('PlaySound(Draw)','False')
-    action('Attack(Sol4,Charlie1)')
-    action('PlaySound(Draw)','Flase')
-    action('Attack(Charlie1,Sol4)')
-    action('Die(Sol4)')
+    attack(charlie_dream.name,soldier4.name)
 
-    action('WalkTo(Charlie1,Ruins.Chest)')
-    action('PutDown(Charlie1,Sword)')
-    action('OpenFurniture(Charlie1,Ruins.Chest)')
+    action('WalkTo('+charlie_dream.name+',Ruins.Chest)')
+    action('PutDown('+charlie_dream.name+',Sword)')
+    action('OpenFurniture('+charlie_dream.name+',Ruins.Chest)')
     action('CreateEffect(Ruins.Chest,Explosion)')
 
+def attack(x,y):
+    action('WalkTo('+x+','+y+')')
+    action('PlaySound(Draw)','False')
+    action('Attack('+x+','+y+')')
+    action('Die('+y+')')
 
 def scene1():
-    action('SetCameraFocus(Charlie)')
+    action('SetCameraFocus('+charlie_earth.name+')')
     action('Wait(1)')
     action('SetNarration(Charlie: Oh boy!.. That was a dream!.)')
     action('ShowNarration')
     action('Wait(3)')
     action('HideNarration')
-    action('Sit(Charlie,Cottage.Bed)')
-    action('SetNarration(Charlie: A boring day starts again)')
+    action('Sit('+charlie_earth.name+',Cottage.Bed)')
+    action('SetNarration('+charlie_earth.name+': A boring day starts again)')
     action('ShowNarration')
     action('Wait(3)')
     action('HideNarration')
