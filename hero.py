841da391a04ef690637fc7e13e58de9e06d17aef
pratheek reddy <40834059+pratheekreddy@users.noreply.github.com>
2021-12-10 00:09:48 -0600

    AG-75

---
+++ hero.py
@@ -0,0 +1,16 @@
+from action import action
+from create_char import Create_char
+class Hero(Create_char):
+    _shared = None
+
+    def __init__(self):
+        raise RuntimeError('Call instance() instead')
+
+    @classmethod
+    def getinstance(cls,name, body_type , clothing ,position,items, hair="Spiky"):
+        if cls._shared is None:
+            cls._shared=cls.__new__(cls)
+            # cls._shared.name=name
+            Create_char.__init__(cls,name,body_type,clothing,position)
+            cls._shared.items=items
+        return cls._shared
